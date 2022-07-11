8c016dd7fd034657a37ca9bbf2ea6c12ee9986d7
lx <lx@atguigu.com>
2022-07-11 12:20:15 +0800

    第二次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git ");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hello " + i);
+        }
     }
 }
