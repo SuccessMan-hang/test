ba7911433a1952761492694efe2682afa10a183f
lx <lx@atguigu.com>
2022-07-11 12:27:49 +0800

    再一次提交

---
+++ src/main/java/Test2.java
@@ -0,0 +1,11 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("xxxxxx");
+
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("xxxoiuu");
+        }
+    }
+}
