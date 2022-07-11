104d874819dd096101fa790f181719f0ee7ca686
lx <lx@atguigu.com>
2022-07-11 14:43:57 +0800

    future-02 a commit

---
+++ src/main/java/Controller/UserController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class UserController {
+
+
+    public String getName(){
+        return "lucy";
+    }
+
+    public void test(){
+
+        System.out.println("hello");
+    }
+}
